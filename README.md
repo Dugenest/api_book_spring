98cf44ce7d5b09ec2aa3f9d6a32e22b6ab831c9c
Dugenest <sebastien.dugenest@gmail.com>
2024-11-22 11:09:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Projet-group-book
---
+++ src/main/java/com/auchan/ApiBookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.auchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBookSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBookSpringApplication.class, args);
+	}
+
+}
